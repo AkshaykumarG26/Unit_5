fcd4c98ebb1d45203df1e472ed23beb4cc8a854b
AkshaykumarG26 <akshayg2697@gmail.com>
2022-01-10 22:53:43 +0530

    .

---
+++ Assignments/28_Dec_21/assignment/src/App.jsx
@@ -0,0 +1,17 @@
+
+import { Table } from './Components/Table';
+import './App.css';
+
+import {FormData} from './Components/EmployeeForm'
+
+function App() {
+  return (
+    <div className="App">
+      <FormData/>
+      <Table/>
+    </div>
+  );
+}
+
+export default App;
+
--- Practicals/28_Dec_21/practical/src/components/Forms.jsx
+++ Practicals/28_Dec_21/practical/src/components/Forms.jsx
@@ -1,33 +1,47 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export const Form = () => {
+  const [form, setForm] = useState({
+    userName: "",
+    age: "",
+  });
 
-    const [form, setForm] = useState({
-        userName: "",
-        age: ""
-    })
+  const handleChange = (e) => {
+    // console.log(e.target.name)
+    const { name, value } = e.target;
 
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
 
-    const handleChange = (e) => {
-        // console.log(e.target.name)
-        const {name, value} = e.target;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(form);
+  };
 
-        setForm({
-            ...form,
-            [name]: value
-        })
-    }
-
-    
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        console.log(form)
-    }
-
-
-    return <form action="" onSubmit={handleSubmit}>
-        <input onChange={handleChange} type="text" name="userName" id="" placeholder="Enter Name"/>
-        <input onChange={handleChange} type="number" name="age" id="" placeholder="Enter Age"/>
-        <input type="submit" name="" id="" />
+  return (
+    <form action="" onSubmit={handleSubmit}>
+      <input
+        onChange={handleChange}
+        type="text"
+        name="userName"
+        id=""
+        placeholder="Enter Name"
+      />
+      <input
+        onChange={handleChange}
+        type="number"
+        name="age"
+        id=""
+        placeholder="Enter Age"
+      />
+      <input type="submit" name="" id="" />
     </form>
-}
+  );
+};
+
+
+
+
